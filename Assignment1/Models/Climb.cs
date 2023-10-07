76370e68a91d92885b293c41a328771bd368ba21
Spencer Cloughley <spencercloughley@gmail.com>
2023-10-07 10:30:54 -0400

    Added models for each gym and climb

---
+++ Assignment1/Models/Climb.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class Climb
+    {
+        public int ClimbId { get; set; }
+        [Required]
+        public string Colour { get; set; }
+        [Required]
+        public string Style { get; set; }
+        [Required]
+        public Boolean Completed { get; set; }
+        [Required]
+        public int GymId { get; set; }
+        [Required]
+        public Gym Gym { get; set; }
+    }
+}
---
+++ Assignment1/Models/Gym.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class Gym
+    {
+        public int GymId { get; set; }
+        [Required]
+        [Display(Name = "Gym Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+        public List<Climb> Climbs { get; set; }
+    }
+}
